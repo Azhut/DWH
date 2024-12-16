b2995ffd6137d2a409e7e37fc8865c31ea2a9f5e
Zhou_zhi <zhora.klem@yandex.ru>
2024-12-17 02:07:10 +0500

    minus \n minus double headers

--- app/features/sheet_parsers/base_sheet_parser.py
+++ app/features/sheet_parsers/base_sheet_parser.py
@@ -1,37 +1,48 @@
-from typing import Tuple
-
 import pandas as pd
 
 class BaseSheetParser:
-    def __init__(self, header_row_range: Tuple[int, int], vertical_header_col: int, start_data_row: int):
+    """
+    Базовый класс для парсинга данных с листов Excel.
+    """
+
+    def __init__(self, header_row_range, vertical_header_col, start_data_row):
+        """
+        Инициализация парсера с конкретными параметрами.
+
+        :param header_row_range: Диапазон строк для верхних заголовков (например, (1, 4))
+        :param vertical_header_col: Индекс колонки для вертикальных заголовков
+        :param start_data_row: Строка, с которой начинаются данные
+        """
         self.header_row_range = header_row_range
         self.vertical_header_col = vertical_header_col
         self.start_data_row = start_data_row
 
-    def parse(self, sheet: pd.DataFrame) -> dict:
-        horizontal_headers, vertical_headers = self._parse_headers(sheet)
-        data = self._create_data(sheet, horizontal_headers, vertical_headers)
-        return {
-            "headers": {
-                "vertical": vertical_headers,
-                "horizontal": horizontal_headers
-            },
-            "data": data
-        }
+    def parse_headers(self, sheet: pd.DataFrame):
+        """
+        Парсит заголовки с листа.
+        """
+        header_rows = self._get_header_rows(sheet)
+        self._fill_empty_cells_in_headers(header_rows)
+        horizontal_headers = self._get_horizontal_headers(header_rows)
+        vertical_headers = self._get_vertical_headers(sheet)
 
-    def _parse_headers(self, sheet: pd.DataFrame) -> Tuple[list, list]:
-        header_rows = self._prepare_header_rows(sheet)
-        horizontal_headers = self._generate_horizontal_headers(header_rows)
-        vertical_headers = self._extract_vertical_headers(sheet)
+
+        horizontal_headers = self._remove_newlines_from_headers(horizontal_headers)
+        vertical_headers = self._remove_newlines_from_headers(vertical_headers)
+
         return horizontal_headers, vertical_headers
 
-    def _prepare_header_rows(self, sheet: pd.DataFrame) -> pd.DataFrame:
-        header_rows = sheet.iloc[self.header_row_range[0]:self.header_row_range[1]].fillna("")
-        self._fill_empty_cells(header_rows)
-        return header_rows
+    def _get_header_rows(self, sheet: pd.DataFrame):
+        """
+        Получает строки заголовков, пропуская ненужные строки.
+        """
+        return sheet.iloc[self.header_row_range[0]:self.header_row_range[1]].fillna("")
 
-    @staticmethod
-    def _fill_empty_cells(header_rows: pd.DataFrame):
+    def _fill_empty_cells_in_headers(self, header_rows: pd.DataFrame):
+        """
+        Заполняет пустые ячейки в строках заголовков.
+        """
+        # Заполнение пустых ячеек с последней строки вверх и слева направо
         for row_idx in range(len(header_rows) - 1, 0, -1):
             for col_idx in range(header_rows.shape[1]):
                 if header_rows.iloc[row_idx, col_idx] == "":
@@ -39,26 +50,41 @@ class BaseSheetParser:
                         if header_rows.iloc[search_row, col_idx] != "":
                             header_rows.iloc[row_idx, col_idx] = header_rows.iloc[search_row, col_idx]
                             break
-        for row_idx in range(len(header_rows)):
+
+        for row_idx in range(self.header_row_range[0]-1, header_rows.shape[0]):
             for col_idx in range(1, header_rows.shape[1]):
                 if header_rows.iloc[row_idx, col_idx] == "":
                     header_rows.iloc[row_idx, col_idx] = header_rows.iloc[row_idx, col_idx - 1]
 
-    def _generate_horizontal_headers(self, header_rows: pd.DataFrame) -> list:
+    def _get_horizontal_headers(self, header_rows: pd.DataFrame):
+        """
+        Формирует уникальные горизонтальные заголовки через разделители.
+        """
         horizontal_headers = []
-        for col_idx in range(1, header_rows.shape[1]):
-            path = []
-            for row_idx in range(len(header_rows) - 1, -1, -1):
+        for col_idx in range(1, header_rows.shape[1]):  # Начинаем с 1-го столбца
+            current_path = []
+            current_value = header_rows.iloc[len(header_rows) - 1, col_idx]
+            current_path.append(current_value)
+
+            for row_idx in range(len(header_rows) - 2, -1, -1):
                 value = header_rows.iloc[row_idx, col_idx]
-                if value:
-                    path.insert(0, value)
-            horizontal_headers.append("$".join(path))
+                if value != current_value:
+                    current_path.insert(0, value)
+                    current_value = value
+
+            horizontal_headers.append("$".join(current_path))
         return horizontal_headers
 
-    def _extract_vertical_headers(self, sheet: pd.DataFrame) -> list:
+    def _get_vertical_headers(self, sheet: pd.DataFrame):
+        """
+        Получает вертикальные заголовки из первой колонки.
+        """
         return sheet.iloc[self.start_data_row:, self.vertical_header_col].dropna().tolist()
 
-    def _create_data(self, sheet: pd.DataFrame, horizontal_headers: list, vertical_headers: list) -> list:
+    def create_data(self, sheet: pd.DataFrame, horizontal_headers: list, vertical_headers: list):
+        """
+        Универсальный метод для создания данных, используя горизонтальные и вертикальные заголовки.
+        """
         data = []
         for col_idx, column_header in enumerate(horizontal_headers, start=1):
             column_values = []
@@ -66,4 +92,24 @@ class BaseSheetParser:
                 cell_value = sheet.iloc[row_idx + self.start_data_row, col_idx]
                 column_values.append({"row_header": row_header, "value": cell_value})
             data.append({"column_header": column_header, "values": column_values})
+
         return data
+
+    def parse(self, sheet: pd.DataFrame):
+        """
+        Основной метод для парсинга.
+        """
+        horizontal_headers, vertical_headers = self.parse_headers(sheet)
+        data = self.create_data(sheet, horizontal_headers, vertical_headers)
+        return {
+            "headers": {
+                "vertical": vertical_headers,
+                "horizontal": horizontal_headers
+            },
+            "data": data
+        }
+    def _remove_newlines_from_headers(self, headers: list):
+        """
+        Удаляет символы новой строки из списка заголовков.
+        """
+        return [header.replace("\n", "") for header in headers]
--- main.py
+++ main.py
@@ -22,4 +22,4 @@ def create_app() -> FastAPI:
 app = create_app()
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload= True)
+    uvicorn.run("main:app", host="0.0.0.0", port=2700, reload= True)
