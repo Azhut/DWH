7f4d81e263cee35a430755d72c9105dd7a906664
Zhou_zhi <zhora.klem@yandex.ru>
2025-05-04 00:48:47 +0500

    Deleted .gitignore files

--- app/features/sheet_parsers/tester.py
+++
@@ -1,6 +0,0 @@
-from app.features.sheet_parsers.HeaderFixer import fix_header
-
-raw = "Численнос\nть\nобучающих\nся,\nотнесенны\nх по\nсостоянию\nздоровья к\nспецмедгру\nппе\n(человек)"
-print(fix_header(raw))
-
-
