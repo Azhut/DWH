1db96a779af518ea3411a312791486a75d4dfcfe
Zhou_zhi <zhora.klem@yandex.ru>
2026-04-06 22:53:06 +0500

    little fixes

--- docker/mongodb/replica-set-init.sh
+++ docker/mongodb/replica-set-init.sh
@@ -26,7 +26,7 @@ try {
     rs.initiate({
         _id: 'rs0',
         members: [
-            { _id: 0, host: 'localhost:27017' }
+            { _id: 0, host: 'mongo:27017' }
         ]
     });
     print('Replica set initiation command sent');
